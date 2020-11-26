80db5db59a2884953836195c32b1d594c66fde21
DennisPierins <dennispierins@gmail.com>
2020-11-26 11:06:33 +0100

    Created main-view, movie-card and movie-view components

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import axios from 'axios';
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+// class MainView extends React.Component {
+//   constructor() {
+//     // Call the superclass constructor so React can initialize it
+//     super();
+//     // Initialize the state to an empty object so we can destructure it later
+//     this.state = {};
+//   }
+
+//   // This overrider the render() method of the superclass. No need to call super(), it does nothing by default
+//   render() {
+//     return (
+//       <div className="main-view"></div>
+//     );
+//   }
+// }
+
+export class MainView extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
+  }
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('https://themyflixapi.herokuapp.com/movies/')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+  render() {
+    // If the state isn't initialized, this will throw on runtime before the data is initially loaded
+    const { movies, selectedMovie } = this.state;
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
+        }
+      </div>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// This together with 'return <MainView/> will render MainView from within index.jsx
+import { MainView } from './components/main-view/main-view';
+
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
-    );
+    return <MainView />;
   }
 }
 
